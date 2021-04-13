bfc4da073166987b86d6630311147174bcbf3d5c
Laura <l.herchmer@gmail.com>
2021-04-13 12:03:44 -0400

    Made some changes, also testing out git

--- route_config.py
+++ route_config.py
@@ -1,23 +1,35 @@
 from flask import Flask
 import sqlite3
 import os
+import json
 
 app = Flask(__name__)
 db_name = "player_services.db"
-
 
 @app.route('/')
 def sayHello():
   return "<h1>Hello</h1>"
 
+# @app.route('/v1/player',methods=['GET'])
+# def get_player_data():
+#   with sqlite3.connect(db_name) as con:
+#     c = con.cursor()
+#   c.execute("SELECT * FROM players")
+#   res = str(c.fetchone())
+#   return res
 
 @app.route('/v1/player',methods=['GET'])
-def get_player_data():
-  with sqlite3.connect(db_name) as con:
-    c = con.cursor()
-  c.execute("SELECT * FROM players")
-  res = str(c.fetchone())
-  return res
+def get_player_data():    
+  with sqlite3.connect(db_name) as conn:
+    cursor = conn.cursor()
+    sqli_query = "SELECT * FROM players"
+  cursor.execute(sqli_query)
+  result = cursor.fetchall()
+  final = []
+  for row in result:
+        item = {'id': row[0], 'name': row[1]}
+        final.append(item)
+  return json.dumps(final)
 
 
 #this is a POST method which stores student details
@@ -29,7 +41,6 @@ def store_student_data():
 @app.after_request
 def after_requestuest(response):
   return response
-
 
 app.debug = True
 host = os.environ.get('OP', '0.0.0.0')
