ec561d73eb92a0f67fab378d0c541719feb8cff3
jlalond1@confederationcollege.ca <jlalond1@confederationcollege.ca>
2021-04-14 11:48:04 -0400

    api 1.1 coded but not yet tested

--- route_config.py
+++ route_config.py
@@ -19,32 +19,28 @@ def sayHello():
 def createNewPlayerCharacter(game_id,player_id):
   #insert character into db
   json_data = Request.json
-  player_id = int(player_id)
-  game_id = json_data["game_id"]
   title = json_data["title"]
   with sqlite3.connect(db_name) as conn:
     cursor = conn.cursor()
     sqli_query = "INSERT INTO characters (game_id, title, player_id) VALUES (?,?,?)"
-    query = cursor.execute(sqli_query,(game_id, title, player_id))
+    query = cursor.execute(sqli_query,(int(game_id), title, int(player_id)))
+    if query != False:#not sure if this if is needed, but i figured if the insert didn't work, I shouldn't get the lastrowid
+      characters_id = cursor.lastrowid #this gets the characters_id
   #if the query failed
   if not query:
     abort(409, "Could not create character")
   
   #build response json
-    #make a query for data needed
     # RESPONSE MODEL - application/json
-    # title string 
-    # id string
-    # game_id string
-    # player_id string
-    # location string
-    # attributes object
-    #build a python data structure (a list of dicts)
+    # title string -- take from above
+    # id string -- take from above
+    # game_id string -- take from above
+    # player_id string -- take from above
+    # location string -- null - because we just made the char
+    # attributes object -- null
 
-
-    #replace the json below here with the json built in the step above
-  
-  return jsonify(jsonData), 201
+  return_json = {"title":title, "id":str(characters_id), "game_id":game_id, "player_id":player_id, "location":null, "attributes":null}
+  return jsonify(return_json), 201
   
 @app.route('/v1/player',methods=['GET'])
 def get_player_data():    
