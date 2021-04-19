b8022011bb239821e0f53a4702e2facf8e725e03
jlalond1@confederationcollege.ca <jlalond1@confederationcollege.ca>
2021-04-19 18:30:29 -0400

    api 1.x updated to psycopg2

--- server.py
+++ server.py
@@ -10,8 +10,10 @@ import psycopg2
 
 app = Flask(__name__)
 
-players_db = os.environ['DATABASE_URL']
-content_db = os.environ['HEROKU_POSTGRESQL_JADE_URL']
+# players_db = os.environ['DATABASE_URL']
+# content_db = os.environ['HEROKU_POSTGRESQL_JADE_URL']
+players_db = "postgres://ccybprhsnhfxgx:b2ce8114ee71abcadd147b42a47b1caa4ae540ac946317a821d04098d40d4acf@ec2-107-20-153-39.compute-1.amazonaws.com:5432/d4b7p3avace43e"
+content_db = "postgres://hbwubwppwmylry:07310537702b404ca848a5c28e8f791fa4bb8abd942f81b8166ea505bf811620@ec2-34-225-103-117.compute-1.amazonaws.com:5432/deml8rlqer0tim"
 
 @app.route('/')
 def sayHello():
@@ -25,15 +27,14 @@ def createNewPlayerCharacter(game_id,player_id):
   content = request.get_json()
   title = content['title']
 
-  with psycopg2.connect(players_db) as conn:
+  with psycopg2.connect(players_db, sslmode='require') as conn:
     cursor = conn.cursor()
-    sqli_query = "INSERT INTO characters (game_id, title, player_id) VALUES (%s,%s,%s)"
-    query = cursor.execute(sqli_query,(int(game_id), title, int(player_id)))
-    if query != False:#not sure if this if is needed, but i figured if the insert didn't work, I shouldn't get the lastrowid
-      characters_id = cursor.lastrowid #this gets the characters_id
-  #if the query failed
-  if not query:
-    abort(409, "Could not create character")
+    sqli_query = "INSERT INTO characters (game_id, title, player_id) VALUES (%s,%s,%s) RETURNING characters_id"
+    cursor.execute(sqli_query,(int(game_id), title, int(player_id)))
+    #if the query failed
+    if cursor.rowcount == 0:
+      return flask.abort(409, "Could not create character")
+    characters_id = cursor.fetchone()[0]
 
   return_json = {"title":title, "id":str(characters_id), "game_id":game_id, "player_id":player_id, "location":"null", "attributes":"null"}
   return jsonify(return_json), 201
@@ -43,10 +44,10 @@ def createNewPlayerCharacter(game_id,player_id):
 #retrieve a list of player characters
 @app.route('/player/<player_id>/character',methods=['GET'])
 def get_player_characters(player_id):  
-  with psycopg2.connect(players_db) as conn:
+  with psycopg2.connect(players_db, sslmode='require') as conn:
     cursor = conn.cursor()
-    sqli_query = "SELECT characters_id, title FROM characters WHERE player_id=%s"
-    cursor.execute(sqli_query, (test,))
+    query = "SELECT characters_id, title FROM characters WHERE player_id=%s"
+    cursor.execute(query, (player_id,))
   result = cursor.fetchall()
   final = []
   for row in result:
@@ -59,26 +60,37 @@ def get_player_characters(player_id):
 #retrieve player character details
 @app.route('/player/<player_id>/character/<characters_id>',methods=['GET'])
 def get_player_characters_details(player_id, characters_id):   
-  with psycopg2.connect(players_db) as conn:
+  with psycopg2.connect(players_db, sslmode='require') as conn:
     cursor = conn.cursor()
-    sqli_query = "SELECT * FROM characters WHERE characters_id=%s" 
-    cursor.execute(sqli_query, (characters_id,))
-  result = cursor.fetchone()
-  result_characters_id = result[0]
-  result_game_id = result[1]
-  result_player_id = result[2]
-  result_title = result[3]
-
-  with psycopg2.connect(players_db) as conn:
-    cursor = conn.cursor()
-    sqli_query = "SELECT * FROM characters_attributes WHERE character_id=%s" 
-    cursor.execute(sqli_query, (characters_id,))  
-  result = cursor.fetchone()
-  result_attributes = {"players_attributes_id":result[0],"player_id":result[1],"attr_title":result[2],"attr_value":result[3]}
+    query = "SELECT * FROM characters WHERE characters_id=%s" 
+    cursor.execute(query, (characters_id,))
+    for record in cursor:
+      result_characters_id = record[0]
+      result_game_id = record[1]
+      result_player_id = record[2]
+      result_title = record[3]
+    query = "SELECT * FROM characters_attributes WHERE character_id=%s" 
+    cursor.execute(query, (characters_id,))
+    result_attributes = {}
+    for record in cursor:
+      item = {record[0]:record[1]}
+      result_attributes.append(item)
   character = {"title":result_title, "id":result_characters_id, "game_id":result_game_id, "player_id":result_player_id, "attributes":result_attributes}
   return jsonify(character), 200
 
-#get 1.4 from Laura
+# API 1.4
+# Delete player character
+@app.route('/player/<player_id>/character/<characters_id>', methods=['DELETE'])
+def deletePlayerCharacter(player_id, characters_id):
+    data = request.get_json()
+    with psycopg2.connect(players_db, sslmode='require') as conn:
+      cursor = conn.cursor()
+      cursor.execute("SELECT characters_id FROM characters WHERE characters_id=%s AND player_id=%s", (characters_id, player_id))
+      if cursor.fetchone():
+        cursor.execute("DELETE FROM characters WHERE characters_id=%s AND player_id=%s", (characters_id, player_id))
+      else:
+        abort(409, "Character does not exist")
+    return jsonify(data), 204
 
 
 # 4.1 - Retrieve all items
@@ -128,7 +140,7 @@ def createNewItem(game_id):
       sqli_query = "INSERT INTO items_aliases (item_id, title) VALUES (%s,%s)"
       query = cursor.execute(sqli_query, (item_id, v))      
       if not query:
-        abort(409, "Could not create item")
+        return flask.abort(409, "Could not create item")
 
   # 3. loop through attributes object, insert items_attr. -> item_id, attr_title, attr_value
   with psycopg2.connect(content_db) as conn:
@@ -217,11 +229,11 @@ def updateItemDetails(game_id,item_id):
 #6.1 Retrieve a list of players
 @app.route('/player', methods=['GET'])
 def get_players():    
-  with psycopg2.connect(player_db, sslmode='require') as conn:
+  with psycopg2.connect(players_db, sslmode='require') as conn:
     cursor = conn.cursor()
-    result = cursor.execute("SELECT * FROM players")
+    cursor.execute("SELECT * FROM players")
   final = []
-  for row in result:
+  for row in cursor:
         item = {'id': row[0], 'title': row[1]}
         final.append(item) 
   return jsonify(final), 201
@@ -310,10 +322,10 @@ def update_players(player_id):
 def after_requestuest(response):
   return response
 
-# app.debug = True
-# host = os.environ.get('OP', '0.0.0.0')
-# port = int(os.environ.get('PORT', 8080))
-# app.run(host=host, port=port)
+app.debug = True
+host = os.environ.get('OP', '0.0.0.0')
+port = int(os.environ.get('PORT', 8080))
+app.run(host=host, port=port)
 
-if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(threaded=True, port=5000)
